05cdcb7a4b3da29fac0c1f265c9a6b51fa46e535
Tillerg <mrgish@yandex.ru>
2022-08-04 10:12:19 +0300

    Cleaned import and rename class

--- src/test/java/SelenideGitTest.java
+++ src/test/java/SelenideGitTest.java
@@ -1,16 +1,12 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class SelenideGit {
+public class SelenideGitTest {
 
     @BeforeAll
     static void config() {
